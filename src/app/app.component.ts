18952aa9caa4446fcc32249a41fe6a56eb7facfa
Pauloglhr <pauloglhr@outlook.com>
2024-11-06 20:05:53 -0300

    Adicionado lógicas iniciais para exibição de uma lista de itens pré-definida em service, no template principal

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,12 +1,20 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { Item } from './interfaces/iItem';
+import { ListaDeCompraService } from './service/lista-de-compra.service';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css']
 })
-export class AppComponent {
+export class AppComponent implements OnInit {
   title = 'app-lista-de-compras';
+  listaDeCompras!: Array<Item>;
+  constructor(private service: ListaDeCompraService) { }
 
-  constructor() { }
+  ngOnInit(): void {
+    this.listaDeCompras = this.service.getListaDeCompra()
+    console.log(this.listaDeCompras);
+    
+  }
 }
